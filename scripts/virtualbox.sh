bceafe68cacb37f2ff69a4e82ff1cbe0ac8b692c
David Asabina <vid@bina.me>
2017-11-16 19:23:32 +0100

    only install guest additions in virtualbox.sh

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-apt-get install -y tasksel linux-headers-$(uname -r) build-essential dkms
-
-#tasksel --list-tasks
-tasksel install ubuntu-desktop
-
 #VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 #VBOX_ISO=VBoxGuestAdditions_$VBOX_VERSION.iso
 
