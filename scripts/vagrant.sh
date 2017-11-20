4448f16bf0654581f18f51bbf456e9e2ed18c929
David Asabina <vid@bina.me>
2017-11-20 04:27:19 +0100

    fixed ssh directory typo

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -8,7 +8,7 @@ usermod -a -G sudo vagrant
 
 mkdir -p /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
-cd /home/vagrant/ssh
+cd /home/vagrant/.ssh
 wget --no-check-certificate 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
 chmod 600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
